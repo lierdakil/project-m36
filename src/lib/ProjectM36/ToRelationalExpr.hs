1260fba5ef24ed11c0accc9cab32d2b2355ca28b
Nikolay Yakimov <root@livid.pp.ru>
2018-12-08 22:49:14 +0300

    Fix some more overlapping instances in ToRelationalExpr
    Uses the trick from https://kseo.github.io/posts/2017-02-05-avoid-overlapping-instances-with-closed-type-families.html

--- src/lib/ProjectM36/ToRelationalExpr.hs
+++ src/lib/ProjectM36/ToRelationalExpr.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances, UndecidableInstances #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, DataKinds, TypeFamilies, MultiParamTypeClasses #-}
 module ProjectM36.ToRelationalExpr where
 
 import ProjectM36.Base
@@ -8,24 +8,36 @@ import ProjectM36.TupleSet
 import Data.Proxy
 import Data.Foldable
 
+type family ToRelationalExprDispatcher a :: Bool where
+  ToRelationalExprDispatcher RelationalExpr = 'True
+  ToRelationalExprDispatcher Relation = 'True
+  ToRelationalExprDispatcher a = 'False
+
 class ToRelationalExpr a where
   toRelationalExpr :: a -> RelationalExpr
 
-instance ToRelationalExpr RelationalExpr where
-  toRelationalExpr val = val
+instance (ToRelationalExprDispatcher a ~ flag, ToRelationalExpr' flag a)
+       => ToRelationalExpr a where
+  toRelationalExpr = toRelationalExpr' (Proxy :: Proxy flag)
 
-instance ToRelationalExpr Relation where
-  toRelationalExpr val = ExistingRelation val
+class ToRelationalExpr' (flag :: Bool) a where
+  toRelationalExpr' :: Proxy flag -> a -> RelationalExpr
 
-instance {-# OVERLAPPABLE #-} (Tupleable a) => ToRelationalExpr a where
-  toRelationalExpr val = either (error . show) Prelude.id $ do
+instance ToRelationalExpr' 'True RelationalExpr where
+  toRelationalExpr' _ val = val
+
+instance ToRelationalExpr' 'True Relation where
+  toRelationalExpr' _ val = ExistingRelation val
+
+instance {-# OVERLAPPABLE #-} (Tupleable a) => ToRelationalExpr' 'False a where
+  toRelationalExpr' _ val = either (error . show) Prelude.id $ do
       let attrs = toAttributes (Proxy :: Proxy a)
       tuples <- mkTupleSet attrs [toTuple val]
       let rel = MakeStaticRelation attrs tuples
       pure rel
 
-instance (Tupleable a, Traversable t) => ToRelationalExpr (t a) where
-  toRelationalExpr vals = either (error . show) Prelude.id $ do
+instance (Tupleable a, Traversable t) => ToRelationalExpr' 'False (t a) where
+  toRelationalExpr' _ vals = either (error . show) Prelude.id $ do
       let attrs = toAttributes (Proxy :: Proxy a)
       tuples <- mkTupleSet attrs $ toList (fmap toTuple vals)
       let rel = MakeStaticRelation attrs tuples
